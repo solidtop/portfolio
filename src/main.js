517a9cae64be06772fed9bfb0f08b76fbced3d67
Axel Asp <solidtop.asp@gmail.com>
2022-12-08 16:15:38 +0100

    Added functionality for API loading

--- src/main.js
+++ src/main.js
@@ -11,3 +11,67 @@ navLinks.forEach(link => {
         document.querySelector('.main-nav').classList.remove('open');   
     });
 });
+
+
+class APIAdapter {
+    async loadData() {
+        const res = await fetch('/static/projects.json');
+        const data = await res.json();
+        return data.projects;
+    }
+}
+
+class ProjectList {
+    async render() {
+        const api = new APIAdapter();
+        const projects = await api.loadData();
+
+        projects.forEach(project => {
+            const card = new ProjectCard(project).render();
+            document.querySelector('.project-list').append(card);
+        });
+    }
+}
+
+class ProjectCard {
+
+    constructor(data) {
+        this.data = data;
+    }
+
+    render() {
+        const item = document.createElement('li');
+        item.classList.add('project');
+
+        const image = document.createElement('img');
+        image.classList.add('project__image');
+        image.src = this.data.image;
+        image.alt = 'Image representation of project';
+        item.append(image);
+
+        const title = document.createElement('project__title');
+        title.classList.add('project__title');
+        title.textContent = this.data.title;
+        item.append(title);
+
+        const line = document.createElement('div');
+        line.classList.add('line');
+        item.append(line);
+
+        const desc = document.createElement('p');
+        desc.classList.add('project__description');
+        desc.textContent = this.data.description;
+        item.append(desc);
+
+        const link = document.createElement('a');
+        link.classList.add('button', 'primary');
+        link.href = this.data.url;
+        link.target = '_blank';
+        link.textContent = 'Look it up';
+        item.append(link);
+
+        return item;
+    }
+}
+
+new ProjectList().render();
